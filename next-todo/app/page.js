f2475dc7a6b8cfe52be3823714f6c0ca2cfe525e
Jake Silberstein <76013728+JakeTheFoot@users.noreply.github.com>
2023-06-16 17:45:44 -0400

    Working on delete effect

--- next-todo/app/page.js
+++ next-todo/app/page.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import data from "@/app/data.json";
 import Checkbox from "@/components/Checkbox";
 import DropdownSelectCatagory from "@/components/DropdownSelectCatagory.jsx";
+import { useSpring, animated } from "react-spring";
 
 const Todo = () => {
   const options = data.categories;
@@ -24,6 +25,13 @@ const Todo = () => {
 
   const [deleteVisible, setDeleteVisible] = useState(false);
   const [buttonY, setButtonY] = useState(0);
+  const [remainingScrollHeight, setRemainingScrollHeight] = useState(0);
+  const [tension, setTension] = useState(150);
+
+  const { y } = useSpring({
+    y: buttonY,
+    config: { tension: tension, friction: 15 },
+  });
 
   // Adding handlers for delete button
   const handleMouseEnter = (event) => {
@@ -43,7 +51,11 @@ const Todo = () => {
     } else {
       console.log("Mouse left towards the bottom");
     }
-    setDeleteVisible(false);
+    setTension(400);
+    setTimeout(() => {
+      setDeleteVisible(false);
+      setTension(150);
+    }, 200);
   };
 
   let height = 0;
@@ -51,11 +63,14 @@ const Todo = () => {
   useEffect(() => {
     const element = document.getElementById("table");
     height = element.clientHeight;
+    setRemainingScrollHeight(
+      element.scrollHeight - element.clientHeight - element.scrollTop
+    );
   }, []);
 
   if (deleteVisible) {
     window.onmousemove = (event) => {
-      setButtonY(event.clientY - 575);
+      setButtonY(event.clientY - 575 - remainingScrollHeight);
     };
   }
 
@@ -182,11 +197,13 @@ const Todo = () => {
             </React.Fragment>
           ))}
         </div>
-        <button
+        <animated.button
           className={`transition-all duration-300 ease-in-out absolute right-[-85px] ${
             deleteVisible ? "flex" : "hidden"
           }`}
-          style={{ top: Math.min(Math.max(160 + buttonY, 70), height + 325) }}
+          style={{
+            top: y.to((y) => Math.min(Math.max(160 + y, 70), height + 325)),
+          }}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -199,7 +216,7 @@ const Todo = () => {
               d="M576 128c0-35.3-28.7-64-64-64H205.3c-17 0-33.3 6.7-45.3 18.7L9.4 233.4c-6 6-9.4 14.1-9.4 22.6s3.4 16.6 9.4 22.6L160 429.3c12 12 28.3 18.7 45.3 18.7H512c35.3 0 64-28.7 64-64V128zM271 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
             />
           </svg>
-        </button>
+        </animated.button>
       </div>
     </div>
   );
